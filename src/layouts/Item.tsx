2f832a9eb52975034b25287dd7e57548b70fa7e0
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-04-10 00:33:52 +0200

    Add item layout

---
+++ src/layouts/Item.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+
+interface ItemProps {
+  children: ReactNode;
+}
+
+export default function Item({ children }: ItemProps) {
+  return (
+    <div className="basis-full lg:basis-1/2 2xl:basis-1/3">
+      <div className="m-2.5 flex min-h-[20rem] items-center justify-center overflow-auto rounded-2xl border-2 border-neutral-200 bg-neutral-100/40 backdrop-blur-md transition-all duration-[.25s] hover:scale-[1.005] hover:border-neutral-300 dark:border-neutral-800 dark:bg-[#0c0c0c] dark:hover:border-neutral-600">
+        {children}
+      </div>
+    </div>
+  );
+}

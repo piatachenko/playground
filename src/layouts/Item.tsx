95a4f11d63b9de63e80882fdb84dfd2c76cf9101
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-04-11 08:04:00 +0200

    Fix item .on overflow

--- src/layouts/Item.tsx
+++ src/layouts/Item.tsx
@@ -6,8 +6,8 @@ interface ItemProps {
 
 export default function Item({ children }: ItemProps) {
   return (
-    <div className="basis-full lg:basis-1/2 2xl:basis-1/3">
-      <div className="m-2.5 flex min-h-[20rem] items-center justify-center overflow-auto rounded-2xl border-2 border-neutral-200 bg-neutral-100/40 backdrop-blur-md transition-[border,transform] duration-[.25s] hover:scale-[1.005] hover:border-neutral-300 dark:border-neutral-800 dark:bg-[#0c0c0c] dark:hover:border-neutral-700">
+    <div className="overflow-auto basis-full lg:basis-1/2 2xl:basis-1/3">
+      <div className="m-2.5 p-5 flex flex-wrap min-h-[20rem] items-center justify-center overflow-auto rounded-2xl border-2 border-neutral-200 bg-neutral-100/40 backdrop-blur-md transition-[border,transform] duration-[.25s] hover:scale-[1.005] hover:border-neutral-300 dark:border-neutral-900 dark:bg-[#0c0c0c]/50 dark:hover:border-neutral-700">
         {children}
       </div>
     </div>

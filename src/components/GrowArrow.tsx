1d85e8e62aff31456b5d6b5f2b79a5b2beef11c9
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-04-11 08:02:00 +0200

    Fix growing arrow

--- src/components/GrowArrow.tsx
+++ src/components/GrowArrow.tsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function GrowArrow() {
   return (
     <>
-      <div className="group relative flex items-center text-3xl font-semibold transition-all duration-[.4s] after:absolute after:-bottom-1 after:left-[.1rem] after:h-0.5 after:w-full after:origin-left after:scale-x-[.94] after:bg-neutral-800 after:transition-all after:duration-[.4s] after:content-[''] hover:cursor-pointer dark:after:bg-neutral-300 dark:hover:after:bg-green-500 hover:after:bg-green-500">
-        <p className="transition-all duration-[.4s] group-hover:text-green-500">
+      <div className="group relative flex select-none items-center pb-1 text-3xl font-semibold transition-all duration-[.4s] after:absolute after:bottom-0 after:left-[.1rem] after:h-0.5 after:w-full after:origin-left after:scale-x-[.94] after:bg-neutral-800 after:transition-all after:duration-[.4s] after:content-[''] hover:cursor-pointer hover:after:bg-green-500 dark:after:bg-neutral-300 dark:hover:after:bg-green-500">
+        <p className="text-neutral-800 transition-all duration-[.4s]  group-hover:text-green-500 dark:text-neutral-300">
           Growing Arrow
         </p>
         <div className="mt-1.5 flex items-center">
-          <div className="-mr-5 ml-2 h-[.2rem] w-2 origin-right translate-y-[0.015rem] scale-x-0 bg-neutral-800 opacity-0 transition-all duration-[.4s] group-hover:w-5 group-hover:scale-x-100 group-hover:bg-green-500 group-hover:opacity-100 dark:bg-neutral-300"></div>
-          <div className="transition-all duration-[.4s] group-hover:fill-green-500">
+          <div className="-mr-[1.15rem] ml-2 h-[.1rem] scale-y-[1.7] translate-y-[.04rem] w-2 bg-neutral-800 opacity-0 transition-all duration-[.4s] group-hover:w-5 group-hover:bg-green-500 group-hover:opacity-100 group-active:w-3 dark:bg-neutral-300"></div>
+          <div className="fill-neutral-800 transition-all duration-[.4s] group-hover:fill-green-500 dark:fill-neutral-300">
             <svg
               width="32"
               height="32"
